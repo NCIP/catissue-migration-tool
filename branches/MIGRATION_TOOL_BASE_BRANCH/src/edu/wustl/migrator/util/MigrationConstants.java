60ec03b71dba0342e1ef1c279be20a8720807827
Sagar Baldwa <sagar_baldwa>
2009-07-03 12:24:48 +0000

    Changes related to ANT script and reading data from migrationInstall.properties file
    SVN-Revision: 132

--- branches/MIGRATION_TOOL_BASE_BRANCH/src/edu/wustl/migrator/util/MigrationConstants.java
+++ branches/MIGRATION_TOOL_BASE_BRANCH/src/edu/wustl/migrator/util/MigrationConstants.java
@@ -1,7 +1,18 @@
 package edu.wustl.migrator.util;
 
-
 public class MigrationConstants
 {
-public static final String unmigratedObjectflag ="migrate.unmigrated.objects";
+	public static final String unmigratedObjectflag ="migrate.unmigrated.objects";
+	public static final String MIGRATION_INSTALL_PROPERTIES_FILE = "./migrationInstall.properties";
+	public static final String JBOSS_HOME = "jboss.home";
+	public static final String CLIENT_SESSION_USER_NAME = "clientSession.username";
+	public static final String CLIENT_SESSION_PASSWORD = "clientSession.password";
+	public static final String PRODUCTION_DATABASE_TYPE = "production.database.type";
+	public static final String PRODUCTION_DATABASE_NAME = "production.database.name";
+	public static final String PRODUCTION_DATABASE_USERNAME = "production.database.username";
+	public static final String PRODUCTION_DATABASE_PASSWORD = "production.database.password";
+	public static final String PRODUCTION_DATABASE_PORT = "production.database.port";
+	public static final String PRODUCTION_DATABASE_HOST = "production.database.host";
+	public static final String MYSQL = "mysql";
+	public static final String ORACLE = "oracle";
 }
