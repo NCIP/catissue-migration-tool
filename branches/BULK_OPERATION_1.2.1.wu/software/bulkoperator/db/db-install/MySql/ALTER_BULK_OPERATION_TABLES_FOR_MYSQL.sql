037147dde15402ee2128b6d1e6bae8e29f27031e
Srikanth Adiga <srikanth.adiga@gmail.com>
2011-05-11 08:35:25 +0000

    corrected the sql Scripts
    SVN-Revision: 320

--- branches/BULK_OPERATION_1.2.1.wu/software/bulkoperator/db/db-install/MySql/ALTER_BULK_OPERATION_TABLES_FOR_MYSQL.sql
+++ branches/BULK_OPERATION_1.2.1.wu/software/bulkoperator/db/db-install/MySql/ALTER_BULK_OPERATION_TABLES_FOR_MYSQL.sql
@@ -1,5 +1,5 @@
 ALTER TABLE  catissue_bulk_operation ADD(CSV_TEMPLATE_TEMP VARCHAR(15000));
-UPDATE catissue_bulk_operation set CSV_TEMPLATE_TEMP=CSV_TEMPLATE
+UPDATE catissue_bulk_operation set CSV_TEMPLATE_TEMP=CSV_TEMPLATE;
 alter TABLE  catissue_bulk_operation DROP column  CSV_TEMPLATE;
 ALTER TABLE  catissue_bulk_operation ADD(CSV_TEMPLATE VARCHAR(15000));
 UPDATE catissue_bulk_operation set CSV_TEMPLATE=CSV_TEMPLATE_TEMP;
