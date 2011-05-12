06e87750906a09e7fa9aab8d74c60bff740fbdc2
Srikanth Adiga <srikanth.adiga@gmail.com>
2011-05-12 13:04:55 +0000

    upgrade script for changing column to longtext
    SVN-Revision: 322

--- software/bulkoperator/db/db-install/MySql/ALTER_BULK_OPERATION_TABLES_FOR_MYSQL.sql
+++ software/bulkoperator/db/db-install/MySql/ALTER_BULK_OPERATION_TABLES_FOR_MYSQL.sql
@@ -1,7 +1,15 @@
-ALTER TABLE  catissue_bulk_operation ADD(CSV_TEMPLATE_TEMP VARCHAR(15000));
+ALTER TABLE  catissue_bulk_operation ADD(CSV_TEMPLATE_TEMP LONGTEXT );
 UPDATE catissue_bulk_operation set CSV_TEMPLATE_TEMP=CSV_TEMPLATE;
 alter TABLE  catissue_bulk_operation DROP column  CSV_TEMPLATE;
-ALTER TABLE  catissue_bulk_operation ADD(CSV_TEMPLATE VARCHAR(15000));
+ALTER TABLE  catissue_bulk_operation ADD(CSV_TEMPLATE LONGTEXT);
 UPDATE catissue_bulk_operation set CSV_TEMPLATE=CSV_TEMPLATE_TEMP;
 ALTER TABLE  catissue_bulk_operation DROP column  CSV_TEMPLATE_TEMP;
+commit;
+
+ALTER TABLE  catissue_bulk_operation ADD(XML_TEMPALTE_TEMP LONGTEXT );
+UPDATE catissue_bulk_operation set XML_TEMPALTE_TEMP=CSV_TEMPLATE;
+alter TABLE  catissue_bulk_operation DROP column  XML_TEMPALTE;
+ALTER TABLE  catissue_bulk_operation ADD(XML_TEMPALTE LONGTEXT);
+UPDATE catissue_bulk_operation set XML_TEMPALTE=XML_TEMPALTE_TEMP;
+ALTER TABLE  catissue_bulk_operation DROP column  XML_TEMPALTE_TEMP;
 commit;
