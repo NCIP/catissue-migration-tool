1f78e6f9b147673de62c356531d5878a8334c50a
Prasad Konka <prasad.konka@nih.gov>
2013-05-31 12:49:25 -0400

    Add copyright header

--- software/bulkoperator/db/db-install/MySql/ALTER_BULK_OPERATION_TABLES_FOR_MYSQL.sql
+++ software/bulkoperator/db/db-install/MySql/ALTER_BULK_OPERATION_TABLES_FOR_MYSQL.sql
@@ -1,3 +1,13 @@
+/*L
+   Copyright Washington University in St. Louis
+   Copyright SemanticBits
+   Copyright Persistent Systems
+   Copyright Krishagni
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/catissue_migration_tool/LICENSE.txt for details.
+L*/
+
 ALTER TABLE  catissue_bulk_operation ADD(CSV_TEMPLATE_TEMP VARCHAR(15000));
 UPDATE catissue_bulk_operation set CSV_TEMPLATE_TEMP=CSV_TEMPLATE
 alter TABLE  catissue_bulk_operation DROP column  CSV_TEMPLATE;
