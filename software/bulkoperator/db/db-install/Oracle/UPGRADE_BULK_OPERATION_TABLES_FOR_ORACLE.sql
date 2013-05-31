3799597f54c57fa6b21cbd0664c9825667425df9
Prasad Konka <prasad.konka@nih.gov>
2013-05-31 12:48:38 -0400

    Add copyright header

--- software/bulkoperator/db/db-install/Oracle/UPGRADE_BULK_OPERATION_TABLES_FOR_ORACLE.sql
+++ software/bulkoperator/db/db-install/Oracle/UPGRADE_BULK_OPERATION_TABLES_FOR_ORACLE.sql
@@ -1,1 +1,11 @@
+/*L
+   Copyright Washington University in St. Louis
+   Copyright SemanticBits
+   Copyright Persistent Systems
+   Copyright Krishagni
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/catissue_migration_tool/LICENSE.txt for details.
+L*/
+
 alter table JOB_DETAILS MODIFY LOG_FILE_NAME varchar2(1024);
